fec139936122864ce2ef686d47229403453f12f9
fuad113 <anfuad96@yahoo.com>
2020-10-28 03:28:04 +0600

    states and calulation variables done

--- Offline 2/Implementation/1505113_JobShopModel.py
+++ Offline 2/Implementation/1505113_JobShopModel.py
@@ -1,6 +1,5 @@
 import heapq
 import random
-import matplotlib.pyplot as plt
 import math
 import lcgrand as lg
 
@@ -16,45 +15,94 @@ stationRouting ={}
 meanServiceTimeForEachStation ={}
 
 
+#total simulation time
+simulationHour = 8
+simulationIteration = 30
 
 
+#expon function
+def expon(mean):
+    return  -mean * math.log(lg.lcgrand(1))
 
+#erlang function
+def erlang(mean):
+    ex1 = expon(mean/2)
+    ex2 = expon(mean/2)
+    return ex1 + ex2
 
-#status of the server IDLE or BUSY
+#status of the machine IDLE or BUSY
 BUSY=1
 IDLE=0
-
-#total simulation time
-total_simulation_time=10000
 
 
 # States and statistical counters
 class States:
-    def __init__(self, k):
-        # States
-        self.queue = []
-        #other variables
-        self.time_last_event= 0.0
-        self.k=k
-        self.num_in_q = 0
-        self.total_time_server_served = 0
+    def __init__(self):
+        #---------------------------------------------------------------------------------------------------------------
+        #queue for every station. so it will be a 2D queue
+        self.queue =[]
+        for i in range(numOfStations):
+            stationOwnqueue =[]
+            self.queue.append(stationOwnqueue)
 
-        #an array of k servers and every server is IDLE initially
-        self.server_array=[]
-        self.area_server_status=[]
-        for i in range(k):
-            self.server_array.append(IDLE)
-            self.area_server_status.append(0.0)
+        #busy machine counter. this array counts the number of busy machines of a station.
+        self.numOfBusyMachines =[]
+        for i in range(numOfStations):
+            self.numOfStations.append(0)
 
-        self.total_time_of_delays =0.0
-        self.area_num_in_q = 0.0
+        #serving counter. It counts every station's completed work number
+        self.everyStationServed=[]
+        for i in range(numOfStations):
+            self.everyStationServed.append(0)
+        #---------------------------------------------------------------------------------------------------------------
 
+        #---------------------------------------------------------------------------------------------------------------
+        # total delay of queues of every station
+        self.totalDelayInQueue = []
+        for i in range(numOfStations):
+            self.totalDelayInQueue.append(0.0)
 
-        # Statistics
-        self.util = 0.0
-        self.avgQdelay = 0.0
-        self.avgQlength = 0.0
-        self.served = 0
+        # average delay of queues of every station
+        self.avgDelayInQueue =[]
+        for i in range(numOfStations):
+            self.avgDelayInQueue.append(0.0)
+
+        # area num of every station queue.It will help to determine average queue length.
+        self.areaNumInQueue = []
+        for i in range(numOfStations):
+            self.areaNumInQueue.append(0.0)
+
+        # average queue length of every station's queue
+        self.avgQueueLength = []
+        for i in range(numOfStations):
+            self.avgQueueLength.append(0.0)
+
+        #---------------------------------------------------------------------------------------------------------------
+
+        #---------------------------------------------------------------------------------------------------------------
+        #cuurent jobs in the system. how many jobs are oning in the system right now.
+        self.numOfOngoingJobsInTheSystem = 0
+
+        #job counter. How many works have been done of each jobs
+        self.jobCounter=[]
+        for i in range(numOfJobTypes):
+            self.jobCounter.append(0)
+
+        #average delay of every job types
+        self.avgDelayPerJob=[]
+        for i in range(numOfJobTypes):
+            self.avgDelayPerJob.append(0.0)
+
+        # total average delay
+        self.avgTotalJobDelay = 0.0
+
+        #counting on average how many jobs were in the system
+        self.avgNumOfJobsInSystem = 0.0
+
+        #---------------------------------------------------------------------------------------------------------------
+
+        # time of the last event
+        self.time_last_event = 0.0
 
     def update(self, sim, event):
         #update the statistical values
@@ -63,52 +111,38 @@ class States:
         time_since_last_event = event.eventTime - self.time_last_event
         self.time_last_event= event.eventTime
 
-        #Update area under number-in-queue function
-        self.area_num_in_q += self.num_in_q * time_since_last_event
+        #updating the average queue length
+        for i in range(numOfStations):
+            self.areaNumInQueue[i] += len(self.queue[i]) * time_since_last_event
 
-        #Update area under server-
-        for i in range(self.k):
-            self.area_server_status[i] += self.server_array[i] * time_since_last_event
-            self.total_time_server_served += self.server_array[i] * time_since_last_event
+        self.avgNumOfJobsInSystem += self.numOfOngoingJobsInTheSystem* time_since_last_event
 
 
     def finish(self, sim):
-        if(self.served == 0):
-            self.avgQdelay=0
-        else:
-            self.avgQdelay = self.total_time_of_delays / self.served
+        #---------------------------------------------------------------------------------------------------------------
+        #calculating average delay in each queue
+        for i in range(numOfStations):
+            if(self.everyStationServed[i] != 0):
+                self.avgDelayInQueue[i] = self.totalDelayInQueue[i]/self.everyStationServed[i]
 
-        self.avgQlength = self.area_num_in_q / sim.now()
+        #calculating average length of queue
+        for i in range(numOfStations):
+            self.avgQueueLength[i] = self.areaNumInQueue[i] / sim.now()
+        #---------------------------------------------------------------------------------------------------------------
 
-        #util will be the mean of all servers util
-        sum=0.0
+        #---------------------------------------------------------------------------------------------------------------
+        #average dealay for each of the jobs
+        for i in range(numOfJobTypes):
+            if (self.jobCounter[i] != 0):
+                self.avgDelayPerJob[i] = self.avgDelayPerJob / self.jobCounter[i]
 
-        for i in range(self.k):
-            self.area_server_status[i]=self.area_server_status[i] / sim.now()
-            sum+=self.area_server_status[i]
+        #calculating overall total delays
+        for i in range(numOfJobTypes):
+            self.avgTotalJobDelay += self.avgDelayPerJob[i] * jobProbabilities[i]
 
-        self.util = sum/self.k
-
-
-    def printResults(self, sim):
-        # DO NOT CHANGE THESE LINES
-        print('Experimental Results:')
-        print('MMk Results: lambda = %lf, mu = %lf, k = %d' % (sim.params.lambd, sim.params.mu, sim.params.k))
-        print('MMk Total customer served: %d' % (self.served))
-        print('MMk Average queue length: %lf' % (self.avgQlength))
-        print('MMk Average customer delay in queue: %lf' % (self.avgQdelay))
-        print('MMk Time-average server utility: %lf' % (self.util))
-
-    def getResults(self, sim):
-        return (self.avgQlength, self.avgQdelay, self.util)
-
-# Write more functions if required
-
-#expon function
-def expon(rate):
-    mean=float( 1 / rate )
-    return  -mean * math.log(lg.lcgrand(1))
-
+        #calculating average num of jobs done in the system
+        self.avgNumOfJobsInSystem = self.avgNumOfJobsInSystem / sim.now()
+        #---------------------------------------------------------------------------------------------------------------
 
 class Event:
     def __init__(self, sim):
@@ -130,15 +164,7 @@ class StartEvent(Event):
         self.sim = sim
 
     def process(self, sim):
-        #Beginning of the service. Server is waiting for an arrival
-        l= sim.params.lambd
-        first_arrival_time = self.eventTime + expon(l)
-
-        #schedule the event in the simulator
-        sim.scheduleEvent(ArrivalEvent(first_arrival_time, sim))
-
-        #Scheduleing the exit event of the simulation
-        sim.scheduleEvent(ExitEvent(total_simulation_time,sim))
+        None
 
 
 
@@ -149,9 +175,7 @@ class ExitEvent(Event):
         self.sim = sim
 
     def process(self, sim):
-        #exit event is scheduled in the start event already
-        time_now= self.eventTime
-        #print("This is the Exit Event at time: ", time_now)
+        None
 
 
 
@@ -163,39 +187,7 @@ class ArrivalEvent(Event):
 
 
     def process(self, sim):
-        #schedule the next arrival
-        time_next_event= sim.now() + expon(sim.params.lambd)
-        sim.scheduleEvent(ArrivalEvent(time_next_event,sim))
-
-        #check all the servers are busy or not.
-        busy_check=0
-        server_index=0
-
-        for i in range(sim.params.k):
-            if(sim.states.server_array[i] == BUSY):
-                busy_check+=1
-            else:
-                busy_check=0
-                server_index=i
-                break
-
-        #all servers are busy. So wait in the queue
-        if(busy_check==sim.params.k):
-            sim.states.num_in_q += 1
-            sim.states.queue.append(sim.now())
-
-        #got an IDLE server
-        else:
-            delay=0.0
-            sim.states.total_time_of_delays+=delay
-
-            #increment the number of customer served
-            sim.states.served += 1
-            sim.states.server_array[server_index]= BUSY
-
-            #schedule a departure fot this arrival
-            time= sim.now() + expon(sim.params.mu)
-            sim.scheduleEvent(DepartureEvent(time,sim,server_index))
+        None
 
 
 class DepartureEvent(Event):
@@ -203,39 +195,16 @@ class DepartureEvent(Event):
         self.eventTime = eventTime
         self.eventType = 'DEPARTURE'
         self.sim = sim
-        self.server_no=server_no
 
 
     def process(self, sim):
-        #check whether the queue is empty. If empty then make that particular server IDLE
-        if(sim.states.num_in_q == 0):
-            sim.states.server_array[self.server_no]=IDLE
-
-        else:
-            #decrease the number of people in queue
-            sim.states.num_in_q -= 1
-
-            #compute the delay
-            delay= sim.now() - sim.states.queue[0]
-            sim.states.total_time_of_delays += delay
-
-            # remove the 1st customer from the waiting queue
-            sim.states.queue.pop(0)
-
-            #increment the num of customer served
-            sim.states.served+=1
-
-            #schedule the departure of the customer
-            time=sim.now() + expon(sim.params.mu)
-            sim.scheduleEvent(DepartureEvent(time,sim,self.server_no))
+        None
 
 
 class Simulator:
     def __init__(self, seed):
         self.eventQ = []
         self.simclock = 0
-        self.seed = seed
-        self.params = None
         self.states = None
 
     def initialize(self):
