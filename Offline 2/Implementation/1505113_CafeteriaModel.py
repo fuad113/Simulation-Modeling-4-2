fc026899fcc5825daec4837d580615406cd103a5
fuad113 <anfuad96@yahoo.com>
2020-10-31 01:44:23 +0600

    Cafeteria states done

--- Offline 2/Implementation/1505113_CafeteriaModel.py
+++ Offline 2/Implementation/1505113_CafeteriaModel.py
@@ -14,11 +14,19 @@ interArrivalTimeBetweenGroups = 30
 simulationDuration = 90*60
 
 counters = ["hotfood" , "sandwich" , "drinks" ]
-counterRouting = {
+NoOfCustomerType =3
+customerTypewiseRouting = {
     "1" : ["hotfood" , "drinks" , "cash"],
     "2" : ["sandwich" , "drinks" , "cash"],
     "3" : ["drinks" , "cash"]
 }
+
+cutomerTypeProbabilities ={
+    "1" : 0.8,
+    "2" : 0.15,
+    "3" : 0.05
+}
+
 
 counterSTMap = {
     "hotfood" : [50 , 120],
@@ -35,7 +43,6 @@ counterACTMap = {
 employeeCounter = {}
 
 
-
 #expon function
 def expon(mean):
     return  -mean * math.log(lg.lcgrand(1))
@@ -44,14 +51,251 @@ def expon(mean):
 # States and statistical counters
 class States:
     def __init__(self):
-        None
+        #defining various parameters to calculating various metrics
+
+        #---------------------------------------------------------------------------------------------------------------
+        #queue for various counters
+        #There will be only 1 queue for hot food and sandwich.
+        #No queue for drinks
+        #No. of queue for the cash counter depends on the num of cashier
+
+        hotfoodQueue = []
+        sandwichQueue =[]
+        #taking drinks queue for avoiding complexity
+        drinksQueue=[]
+        #cash counter queue calculation
+        cashCounterEmployeeNo = employeeCounter["cash"]
+        cashQueue=[]
+
+        for i in range(cashCounterEmployeeNo):
+            tempQueue =[]
+            cashQueue.append(tempQueue)
+
+        #Now create a map for tracking various types of queue
+        self.queue={
+            "hotfood" : [hotfoodQueue],
+            "sandwich" : [sandwichQueue],
+            "drinks" : [drinksQueue],
+            "cash" : [cashQueue]
+        }
+        #---------------------------------------------------------------------------------------------------------------
+
+        #---------------------------------------------------------------------------------------------------------------
+        #How many servers/employee at each station available
+        hotfoodServers = employeeCounter["hotfood"]
+        sandwichServers = employeeCounter["sandwich"]
+        drinksServers = np.inf
+        cashServers = employeeCounter["cash"]
+
+        self.serverAvailable = {
+            "hotfood" : hotfoodServers,
+            "sandwich" : sandwichServers,
+            "drinks" : drinksServers,
+            "cash" : cashServers
+        }
+        #---------------------------------------------------------------------------------------------------------------
+
+        #---------------------------------------------------------------------------------------------------------------
+        #special work for the cash counter.
+        #because there can be more than 1 queue in the cash counter
+        self.cashServer =[]
+        for i in range(cashServers):
+            self.cashServer.append(0)
+        #---------------------------------------------------------------------------------------------------------------
+
+        #---------------------------------------------------------------------------------------------------------------
+        #avg and maximum delay for each counter
+        self.avgQDelay = {
+            "hotfood" : 0.0,
+            "sandwich" : 0.0,
+            "cash" : 0.0
+        }
+
+        self.maxQDelay = {
+            "hotfood": 0.0,
+            "sandwich": 0.0,
+            "cash": 0.0
+        }
+        #---------------------------------------------------------------------------------------------------------------
+
+        #---------------------------------------------------------------------------------------------------------------
+        self.overallAvgDelay =0.0
+        #---------------------------------------------------------------------------------------------------------------
+
+        #---------------------------------------------------------------------------------------------------------------
+        #avg and maximum queue length for each counter
+        self.avgQLength = {
+            "hotfood" : 0.0,
+            "sandwich" : 0.0,
+            "cash" : 0.0
+        }
+
+        self.maxQLength = {
+            "hotfood": 0.0,
+            "sandwich": 0.0,
+            "cash": 0.0
+        }
+        #---------------------------------------------------------------------------------------------------------------
+
+        #---------------------------------------------------------------------------------------------------------------
+        #avg and maximum delay for various types(here 3) of customers
+        self.avgCustomerTypewiseDelay ={
+            "1" : 0.0,
+            "2" : 0.0,
+            "3" : 0.0
+        }
+
+        self.maxCustomerTypewiseDelay = {
+            "1": 0.0,
+            "2": 0.0,
+            "3": 0.0
+        }
+        #---------------------------------------------------------------------------------------------------------------
+
+        #---------------------------------------------------------------------------------------------------------------
+        #which counter served how many customers
+        self.customerServed ={
+            "hotfood" : 0,
+            "sandwich" : 0,
+            "drinks" : 0,
+            "cash" : 0
+        }
+
+        #which counter served how many customers typewise
+        self.customerServedTypeWise = {
+            "1" : 0,
+            "2" : 0,
+            "3" : 0
+        }
+        #---------------------------------------------------------------------------------------------------------------
+
+        #---------------------------------------------------------------------------------------------------------------
+        self.time_last_event = 0.0
+        self.currentCustomersIntheSystem =0
+        self.maxCustomerInTheSystem =0
+        self.avgCustomerInTheSystem =0
+        #---------------------------------------------------------------------------------------------------------------
+
 
     def update(self, sim, event):
-        None
+        #update the statistical values
+
+        #Compute time since last event, and update last-event-time- marker
+        time_since_last_event = event.eventTime - self.time_last_event
+        self.time_last_event= event.eventTime
+
+        #update the value of average queue length
+
+        for key in range(self.avgQLength):
+            numOfQueues = len(self.queue[key])
+            qCounter =0
+            for i in range(len(self.queue[key])):
+                currentQueueLength = len(self.queue[key][i])
+                qCounter += currentQueueLength
+                self.maxQLength[key] = max( self.maxQLength[key] , currentQueueLength )
+            self.avgQLength += (qCounter/numOfQueues) * time_since_last_event
+
+
+        #update the customer numbers in the system
+        self.avgCustomerInTheSystem += self.currentCustomersIntheSystem * time_since_last_event
+        self.maxCustomerInTheSystem = max(self.maxCustomerInTheSystem , self.currentCustomersIntheSystem)
 
 
     def finish(self, sim):
-        None
+        #---------------------------------------------------------------------------------------------------------------
+        #Calculation for the Queues
+        #calculation of average queue length
+
+        for key in range(self.avgQLength):
+            self.avgQLength[key] /= sim.now()
+
+        #calculation for the avg queue delay
+        for key in range(self.avgQDelay):
+            #division by 60 is for conversion to per minutes
+            self.avgQDelay[key] =  (self.avgQDelay[key]/self.cutomerServed[key]) / 60
+
+        #---------------------------------------------------------------------------------------------------------------
+
+        #---------------------------------------------------------------------------------------------------------------
+        #calculation for the customer types
+
+        for i in range (NoOfCustomerType):
+
+            key = str(i+1)
+            self.avgCustomerTypewiseDelay[key] /= self.customerServedTypeWise[key]
+            # division by 60 is for conversion to per minutes
+            self.avgCustomerTypewiseDelay[key] /= 60
+            self.avgCustomerTypewiseDelay[key] = round(self.avgCustomerTypewiseDelay[key] , 3 )
+            self.overallAvgDelay += self.avgCustomerTypewiseDelay[key]
+
+        #---------------------------------------------------------------------------------------------------------------
+
+        #average customer in the system
+        self.avgCustomerInTheSystem = self.avgCustomerInTheSystem / sim.now()
+
+        for key in self.maxCustomerTypewiseDelay:
+            # division by 60 is for conversion to per minutes
+            self.maxCustomerTypewiseDelay[key] /= 60
+
+        #---------------------------------------------------------------------------------------------------------------
+        #rounding each values to precision 3
+        for key in range(self.avgQLength):
+            self.avgQLength[key] = round(self.avgQLength[key], 3 )
+
+        for key in range(self.avgQDelay):
+            self.avgQDelay[key] = round(self.avgQDelay[key], 3)
+
+        self.overallAvgDelay = round(self.overallAvgDelay, 3)
+
+        self.avgCustomerInTheSystem = round( self.avgCustomerInTheSystem , 3)
+
+        for key in self.maxCustomerTypewiseDelay:
+            self.maxCustomerTypewiseDelay[key] = round(self.maxCustomerTypewiseDelay[key] , 3)
+
+        for key in self.maxQLength:
+            self.maxQLength[key] = round( self.maxQLength[key] , 3 )
+
+        for key in self.maxQDelay:
+            self.maxQDelay[key] = round(self.maxQDelay[key] , 3 )
+
+        #---------------------------------------------------------------------------------------------------------------
+
+    def results(self):
+        #print all the results
+
+        print("Average No. of Customers in the System: ", self.avgCustomerInTheSystem)
+
+        print("Max No. of Customers in the System: ", self.maxCustomerInTheSystem)
+
+        print("Total Customer served: ", self.customerServed["cash"])
+
+        print("\n\n")
+
+        print("Average Queue Length of Every Counter")
+        print(self.avgQLength)
+
+        print("Max Queue Length of Every Counter")
+        print(self.maxQLength)
+
+        print("\n")
+
+        print("Average Delay in Oueue of Every Counter")
+        print(self.avgQDelay)
+
+        print("Max Delay in Oueue of Every Counter")
+        print(self.maxQDelay)
+
+        print("\n")
+
+        print("Average Delay for Each Type of Customers")
+        print(self.avgCustomerTypewiseDelay)
+        print("\n")
+
+        print("Max Delay for Each Type of Customers")
+        print(self.maxCustomerTypewiseDelay)
+
+        print("Overall Average Delay: " , self.overallAvgDelay)
+
 
 
 class Event:
